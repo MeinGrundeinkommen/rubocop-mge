1a88a799cecd39ea42864a70ab43c11cbd7462ab
Paul Süßenbach <paul@mein-grundeinkommen.de>
2020-10-30 13:37:49 +0100

    Readd inject

--- lib/rubocop-mge.rb
+++ lib/rubocop-mge.rb
@@ -2,5 +2,9 @@
 
 require 'rubocop'
 
+require_relative 'rubocop/mge'
 require_relative 'rubocop/mge/version'
+require_relative 'rubocop/mge/inject'
 require_relative 'rubocop/cop/security/no_mail_parameter'
+
+RuboCop::Mge::Inject.defaults!
